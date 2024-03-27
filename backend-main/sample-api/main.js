f1f41dc6dc1653fa93e2f24fd3a0370e299bb29c
Indra <115783796+IngaIndra@users.noreply.github.com>
2024-03-27 11:01:26 +0800

    some changes

--- backend-main/sample-api/main.js
+++ backend-main/sample-api/main.js
@@ -8,45 +8,54 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/articles", (req, res) => {
-  const data = fs.readFileSync("articles.json", "utf8");
+app.get("/tasks", (req, res) => {
+  const data = fs.readFileSync("tasks.json", "utf8");
   res.json(JSON.parse(data));
 });
 
-app.post("/articles/create", (req, res) => {
-  const { title, desc } = req.body;
+app.post("/tasks/create", (req, res) => {
+  const { title, description } = req.body;
 
-  const data = fs.readFileSync("articles.json", "utf8");
+  const data = fs.readFileSync("tasks.json", "utf8");
   const list = JSON.parse(data);
 
-  const articleId = list.length + 1;
+  const taskId = Date.now();
 
   list.push({
-    id: articleId,
+    id: taskId,
     title: title,
-    desc: desc,
+    description: description,
   });
 
-  fs.writeFileSync("articles.json", JSON.stringify(list));
+  fs.writeFileSync("tasks.json", JSON.stringify(list));
   res.json([{ status: "Success" }]);
 });
 
-app.put("/articles/update/:id", (req, res) => {
-  const id = req.params.id;
-  console.log(id);
+app.put("/tasks/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
 
-  const updateData = req.body;
-  // {
-  //   title: updateData.title,
-  //   desc: updateData.desc,
-  // }
+  const data = fs.readFileSync("tasks.json", "utf8");
+  const list = JSON.parse(data);
+
+  const index = list.findIndex((item) => item.id === Number(id));
+
+  list[index].title = title;
+  fs.writeFileSync("tasks.json", JSON.stringify(list));
 
   res.json([{ status: "Success" }]);
-  // TODO
 });
 
-app.delete("/articles/delete/:id", (req, res) => {
-  // TODO
+app.delete("/tasks/delete/:id", (req, res) => {
+  const { id } = req.params;
+
+  const data = fs.readFileSync("tasks.json", "utf8");
+  const list = JSON.parse(data);
+
+  const newList = list.filter((item) => item.id !== Number(id));
+
+  fs.writeFileSync("tasks.json", JSON.stringify(list));
+  res.json([{ status: "Success" }]);
 });
 
 app.listen(port, () => {
--- backend-main/sample-ui/pages/index.js
+++ backend-main/sample-ui/pages/index.js
@@ -1,24 +1,93 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [articles, setArticles] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  function loadTasks() {
+    axios("http://localhost:3000/tasks").then((response) =>
+      setTasks(response.data)
+    );
+  }
 
   useEffect(() => {
-    fetch("http://localhost:3000/articles")
-      .then((res) => res.json())
-      .then((data) => setArticles(data));
+    loadTasks();
   }, []);
+
+  function createNewTask() {
+    if (!title || !description) {
+      alert("Please enter task");
+      return;
+    }
+    try {
+      axios
+        .post("http://localhost:3000/tasks/create", {
+          title,
+          description,
+        })
+        .then(() => {
+          setTitle("");
+          setDescription("");
+          loadTasks();
+        });
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred while creating the new task.");
+    }
+  }
+
+  function editTask() {
+    console.log(title);
+  }
+
+  function deleteTask() {
+    console.log(title);
+  }
+
+  const handleChangeTitle = (sdf) => {
+    console.log(sdf);
+    setTitle(sdf.target.value);
+  };
+
+  const handleChangeDescription = (event) => {
+    setDescription(event.target.value);
+  };
 
   return (
     <main className="container mx-auto my-10 ">
-      <button className="mb-2 btn btn-primary">New task</button>
-      {articles.map((article) => (
-        <div key={article.id} className="mb-2 shadow card bg-base-100">
+      <div className="mb-3">
+        <input
+          type="text"
+          className="mr-2 input"
+          placeholder="Title"
+          value={title}
+          onChange={handleChangeTitle}
+        />
+        <input
+          type="text"
+          className="mr-2 input"
+          placeholder="description"
+          value={description}
+          onChange={handleChangeDescription}
+        />
+        <button className="btn btn-primary" onClick={createNewTask}>
+          New task
+        </button>
+      </div>
+      {tasks.map((task) => (
+        <div key={task.id} className="mb-5 shadow card bg-base-100">
           <div className="card-body">
             <div className="flex items-center">
-              <div className="flex-1">{article.title}</div>
-              <button className="btn btn-ghost btn-sm">Edit</button>
-              <button className="btn btn-ghost btn-sm">Delete</button>
+              <div className="flex-1">{task.title}</div>
+              <div className="flex-1">{task.description}</div>{" "}
+              <button className="btn btn-ghost btn-sm" onClick={editTask}>
+                Edit
+              </button>
+              <button className="btn btn-ghost btn-sm" onClick={deleteTask}>
+                Delete
+              </button>
             </div>
           </div>
         </div>
